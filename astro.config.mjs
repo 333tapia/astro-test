9dc885e1ea5ec949209aac8d59ce85c407963222
333tapia <333tapia.fernando@gmail.com>
2024-09-29 09:17:37 -0500

    play with tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://eclectic-marigold-1c3f08.netlify.app/',
+});
